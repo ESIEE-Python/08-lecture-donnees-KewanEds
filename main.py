ac2f23ba88ce8d02f169070554ee69215999502b
KewanEds <kewan.edragas@edu.esiee.fr>
2024-12-12 11:00:55 +0000

    Kewan EDRAGAS

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 #### Imports et définition des variables globales
-import random
-
+""" Ex 08 : lectures de données
+"""
 FILENAME = "listes.csv"
 
 #### Fonctions secondaires
-
 def read_data(filename):
     """retourne le contenu du fichier <filename>
 
@@ -14,40 +13,46 @@ def read_data(filename):
     Returns:
         list: le contenu du fichier (1 list par ligne)
     """
-    l = []
+    l=[]
+    with open(filename, mode='r', encoding='utf8') as f :
+        for line in f:
+            l.append([int(value) for value in line.strip().split(';')])
     return l
 
 def get_list_k(data, k):
-    l = []
+    """ retourne la liste d'indice k"""
+    l = data[k]
     return l
 
 def get_first(l):
-    return None
+    """ retourne le premier élément de la liste"""
+    return l[0]
 
 def get_last(l):
-    return None
+    """ retourne le dernier élément de la liste"""
+    return l[len(l)-1]
 
 def get_max(l):
-    return None
+    """retourne le maximum de la liste"""
+    return max(l)
 
 def get_min(l):
-    return None
+    """retourne le minimum de la liste"""
+    return min(l)
 
 def get_sum(l):
-    return None
-
+    """retourne la somme de la liste"""
+    return l.get_first + get_sum(l.pop(0))
 
 #### Fonction principale
-
-
 def main():
-    pass
-    # data = read_data(FILENAME)
-    # for i, l in enumerate(data):
-    #     print(i, l)
-    # k = 37
-    # print(k, get_list_k(data, 37))
-
+    """test et vérifie les fonctions secondaire
+    """
+    data = read_data(FILENAME)
+    for i, l in enumerate(data):
+        print(i, l)
+    k = 37
+    print(k, get_list_k(data, 37))
 
 if __name__ == "__main__":
     main()
